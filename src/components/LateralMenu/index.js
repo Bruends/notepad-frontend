1bf108bb6868bf44218ec37f03efc0aacb36cc85
Bruno Mendes <brunomendes962@gmail.com>
2018-03-14 17:37:47 -0300

    fixing little issues

--- src/components/LateralMenu/index.js
+++ src/components/LateralMenu/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import NotesIcon from 'react-icons/lib/md/developer-board';
-import EditIcon from 'react-icons/lib/md/add-circle-outline';
+import AddIcon from 'react-icons/lib/md/add-circle-outline';
 import LogoutIcon from 'react-icons/lib/md/power-settings-new';
 
 import './lateralMenu.css';
@@ -18,17 +18,17 @@ class LateralMenu extends Component {
       <nav className="lateralMenu_container">
         <ul className="lateralMenu">
           <li className="lateralMenu_item">
-            <a href="">
+            <a href="" title="All Notes">
               <NotesIcon />              
             </ a>
           </li>
           <li className="lateralMenu_item">
-          <a href="">
-              <EditIcon />
+          <a href="" Title="add Note">
+              <AddIcon />
             </a>
           </li>
           <li className="lateralMenu_item">
-            <a href="" onClick={this.logout}>
+            <a href="" title="logout" onClick={this.logout}>
               <LogoutIcon />
             </a>
           </li>
